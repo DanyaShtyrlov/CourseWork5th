5434f19e60087054808e48240bf39a7e2e4b548c
DanyaShtyrlov <137157657+DanyaShtyrlov@users.noreply.github.com>
2024-11-25 02:50:14 +0300

    Add files via upload

--- CourseWorkInterface.py
+++ CourseWorkInterface.py
@@ -10,7 +10,7 @@ main_window.title("Успеваемость учащихся")
 main_window.geometry("1280x720")
 main_window.resizable(False, False)
 
-student_columns = ("ID", "Full_name", "Birth_date", "Study_class", "Student_photo")
+student_columns = ("ID", "Full_name", "Birth_date", "Study_class")
 
 database_viewport = ttk.Treeview(main_window, show="headings", columns=student_columns)
 
@@ -18,9 +18,33 @@ database_viewport.heading("ID", text="№")
 database_viewport.heading("Full_name", text="ФИО")
 database_viewport.heading("Birth_date", text="Дата рождения")
 database_viewport.heading("Study_class", text="Класс, группа")
-database_viewport.heading("Student_photo", text="Фото")
+
 
 database_viewport.grid(row=1, column=0, sticky=NW)
+
+def write_to_file(data, filename):
+    with open(filename, 'wb') as file:
+        file.write(data)
+
+def convert_to_binary_data(filename):
+    with open(filename, 'rb') as file:
+        blob_data = file.read()
+    return blob_data
+
+def get_image_from_database(id):
+        cursor.execute("SELECT Student_photo FROM Student WHERE ID = {0}".format(id))
+        photo = cursor.fetchall()
+        photo = photo[0][0]
+        write_to_file(photo, "C:/Users/Даниил/Desktop/2.png")
+
+def show_image_in_canvas():
+    global database_image
+    current_id = database_viewport.item(database_viewport.focus())["values"][0]
+    get_image_from_database(current_id)
+    database_image = PhotoImage(file="C:/Users/Даниил/Desktop/2.png")
+    database_image_canvas.create_image(10, 10, anchor=NW, image=database_image)
+
+
 
 def add_student_data_to_database():
 
@@ -57,12 +81,12 @@ def add_student_data_to_database():
 
 def refresh_viewport_data():
     cursor.execute("SELECT * FROM {0}".format(choice_table_combobox.get()))
-    student_rows = cursor.fetchall()
+    rows = cursor.fetchall()
 
     for i in database_viewport.get_children():
         database_viewport.delete(i)
 
-    for row in student_rows:
+    for row in rows:
         database_viewport.insert("", END, values=row)
 
 
@@ -75,6 +99,13 @@ add_button = ttk.Button(main_window, text="Добавить", command=add_studen
 add_button.grid(row=0, column=1, sticky=NE)
 refresh_button = ttk.Button(main_window, text="Обновить", command=refresh_viewport_data)
 refresh_button.grid(row=0, column=2, sticky=N)
+image_button = ttk.Button(main_window, text="Показать", command=show_image_in_canvas)
+image_button.grid(row=0, column=3)
+
+
+database_image_canvas = Canvas(main_window, bg="white", width=400, height=400)
+database_image_canvas.grid(row=2, column=0, sticky=W)
+
 
 main_window.mainloop()
 connection.close()
